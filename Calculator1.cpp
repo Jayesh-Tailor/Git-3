92f9fedfae29d5b40949699df6f696b596710006
Jayesh Tailor <tailorjayesh592@gmail.com>
2024-06-05 21:36:26 +0530

    Invalid operations are removed

--- Calculator1.cpp
+++ Calculator1.cpp
@@ -13,7 +13,8 @@ int main(){
     cout<<"Sbstraction is "<<(a-b)<<endl;
     cout<<"Multiplication is "<<(a*b)<<endl;
     cout<<"Division is "<<(a/b)<<endl;
-    cout<<"Remainder is "<<(a%b)<<endl;
+    //cout<<"Remainder is "<<(a%b)<<endl;
+    cout<<"Entered Numbers are [(0 = not equal)(1 = equal)]"<<endl<<(a==b)<<endl;
 
 
     return 0;
