b1a8a6d311d6e33868862c2a9d619964f6a3389e
Jayesh Tailor <tailorjayesh592@gmail.com>
2024-06-05 21:07:01 +0530

    I have made a Calculator for very basic operations(All at a time)

---
+++ Calculator1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b;
+
+    cout<<"Enter a ";
+    cin>>a;
+    cout<<"Enter b ";
+    cin>>b;
+
+    cout<<"addition is "<<(a+b)<<endl;
+    cout<<"Sbstraction is "<<(a-b)<<endl;
+    cout<<"Multiplication is "<<(a*b)<<endl;
+    cout<<"Division is "<<(a/b)<<endl;
+
+
+    return 0;
+}
